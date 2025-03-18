bddeedb963b028fff87aaf24a79f261aba035e3e
deepakrajput56 <41770332+deepakrajput56@users.noreply.github.com>
2025-03-18 23:32:09 +0530

    Update flipkart_steps_update.py
    this is v2

--- features/steps/flipkart_steps.py
+++ features/steps/flipkart_steps.py
@@ -1,4 +1,5 @@
 import yaml
+import time 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
